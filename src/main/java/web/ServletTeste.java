e90b76001de5b6d2a6e13a9514c07a84f018406e
Alisson Youssf <youssfbr@gmail.com>
2018-06-06 22:54:29 -0300

    Primeiro commit

---
+++ src/main/java/web/ServletTeste.java
@@ -0,0 +1,25 @@
+package web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ServletTeste")
+public class ServletTeste extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+       public ServletTeste() {
+   
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
